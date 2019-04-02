3b29acb0570967dc6e7102cc410991995499eb6d
Thomas Taylor <tt35@st-andrews.ac.uk>
2019-04-02 22:04:36 +0100

    Added frontend for displaying the history of a specified room

--- client/app.py
+++ client/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, jsonify, make_response
 from test_data import Rooms
+from test_history import JackColeHistory, JohnHoneyHistory
 from datetime import datetime
 
 app = Flask(__name__)
 
 Rooms = Rooms()
+JC = JackColeHistory()
+JH = JohnHoneyHistory()
 
 
 @app.route('/')
@@ -14,7 +17,7 @@ def current():
 
 @app.route('/history')
 def history():
-    return render_template('history.html')
+    return render_template('history.html', rooms=Rooms, jc=JC, jh=JH)
 
 
 if __name__ == '__main__':
