4f9eb51b74d1365d38cff51e32d7ce6f3fbb1863
Thomas Taylor <tt35@st-andrews.ac.uk>
2019-04-02 19:32:50 +0100

    Added test_data file and markup for its contents; this will later be replaced by calls to the database via the server

--- client/app.py
+++ client/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, jsonify, make_response
-
+from test_data import Rooms
 from datetime import datetime
 
 app = Flask(__name__)
 
+Rooms = Rooms()
+
 
 @app.route('/')
 def current():
-    return render_template('current.html')
+    return render_template('current.html', rooms=Rooms)
 
 
 @app.route('/history')
---
+++ client/test_data.py
@@ -0,0 +1,14 @@
+def Rooms():
+    rooms = [
+        {
+            'id': 1,
+            'name': 'Jack Cole',
+            'temperature': 21
+        },
+        {
+            'id': 2,
+            'name': 'John Honey',
+            'temperature': 22.5
+        }
+    ]
+    return rooms
