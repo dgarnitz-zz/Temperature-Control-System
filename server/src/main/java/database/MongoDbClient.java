b1c154632e140cd4162e7bc3ec2169d73f3275ac
dgarnitz <david.garnitz@gmail.com>
2019-04-04 20:51:42 +0100

    deployed MongoDB to heroku using mLab and connected java API to backend

--- server/src/main/java/database/MongoDbClient.java
+++ server/src/main/java/database/MongoDbClient.java
@@ -11,17 +11,18 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 
 import org.bson.Document;
 
 public class MongoDbClient {
 
-    MongoClient client = new MongoClient();
-    MongoDatabase db = client.getDatabase("tcs");
+    MongoClient client = new MongoClient(new MongoClientURI("mongodb://tcs-admin:password1@ds231956.mlab.com:31956/heroku_z0fxw42s"));
+    MongoDatabase db = client.getDatabase("heroku_z0fxw42s");
 
-    MongoCollection<Document> labs = db.getCollection("labs");
-    MongoCollection<Document> readings = db.getCollection("readings");
+    MongoCollection<Document> test = db.getCollection("test");
+    //MongoCollection<Document> readings = db.getCollection("readings");
 
     /**
      * Save the object into the database.
@@ -34,6 +35,6 @@ public class MongoDbClient {
 
     public void queryAll() {
       System.out.println("Query:");
-      System.out.println(labs.find().first());
+      System.out.println(test.find().first());
     }
 }
